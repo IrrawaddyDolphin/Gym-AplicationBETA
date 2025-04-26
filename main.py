3053f35be85ffb72eb8f5b23215cba7fcaa8e4fb
Dawid <dawid.dorobisz003@gmail.com>
2025-04-26 13:25:04 +0200

    new choice option about inputing your soreness and stress levels, new update in smart_goal, function outputting intensity and notes, GOAL of this function is to decide how ready you are depends of this parameters

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sqlite3
 from beta_plan_generator import whats_goal,level,generate_plan
 from smart_goal import get_true_goal
+from datetime import datetime, timedelta
 def image():
     info=sqlite3.connect("daneUzytkownika.db")
     cursor=info.cursor()
@@ -55,6 +56,8 @@ def image_trainings():
         duration INTEGER,
         intensity INTEGER,
         addictional_notes TEXT,
+        soreness_level INTEGER,
+        stress_level INTEGER,
         FOREIGN KEY(userId) REFERENCES user_info(id)
                    )
 """)
@@ -97,13 +100,39 @@ def get_info_about_user_goal(userId):
     info.close()
 
     return result
+def input_soreness_and_stress_lvl(date):
+    image_trainings()
+    #soreness=float(input("What's your soreness for today? (1-10): "))
+    #stress=float(input("How would you describe your stress levels thruout last 3 days: (1-10): "))
+    sorenessstress=sqlite3.connect("trainingHistory.db")
+    cursor=sorenessstress.cursor()
+    cursor.execute("SELECT * FROM training_history WHERE date = ?",(date,))
+    wynik=cursor.fetchone()
+    if wynik:
+        soreness=float(input("What's your soreness for today? (1-10): "))
+        stress=float(input("How would you describe your stress levels thruout last 3 days: (1-10): "))
+        cursor.execute("""UPDATE training_history SET soreness_level = ?, stress_level = ?
+                    WHERE date = ?
+        """,(soreness,stress,date))
+    else:
+        userId=int(input("What's your user's ID? "))
+        soreness=float(input("What's your soreness for today? (1-10): "))
+        stress=float(input("How would you describe your stress levels thruout last 3 days: (1-10): "))
+        cursor.execute("""INSERT INTO training_history(userId,date,soreness_level, stress_level)
+        VALUES(?,?,?,?)
+                   """,(userId,date,soreness,stress))
+    sorenessstress.commit()
+    sorenessstress.close()
 
 print("1. Add user")
 print("2. Add training")
 print("3. Generate plan for me")
+print("4. Update your soreness and stress levels")
 choice=input("What would you like to do? (type number) ")
 if choice=="1":
     addUser()
+    date=input("Date: ")
+    input_soreness_and_stress_lvl(date)
 elif choice=="2":
     id=int(input("What's your id? "))
     addTraining(id)
@@ -116,3 +145,12 @@ elif choice=="3":
     #goal=whats_goal(goal1)
     goal2=get_true_goal(goal1)
     print(generate_plan(users_lvl,goal2))
+#For now lets start with 3 levels of "being ready" fatiqued/mid ready/ready
+#generate_plan should return training for each lvl [streching/from main sets -1 set/go full in]
+#input=h of sleep, fatique, stress, kcal from previous day x/2600(for example)
+elif choice=="4":
+    if input("Do you want to input other date then yesterdays? Y/N ")=="Y":
+        date=input("Date: ")
+    else:
+        date=(datetime.today()-timedelta(days=1)).strftime("%Y-%m-%d")
+    input_soreness_and_stress_lvl(date)
